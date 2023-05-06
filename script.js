a64de88fbb67a584ae8728374fe546c0fdd4c427
ZhaoJingyan0219 <126554030+ZhaoJingyan0219@users.noreply.github.com>
2023-05-06 14:47:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.write("hello,world");
+function sayHello(){
+  var response = prompt("What is your name?");
+  alert("Hello" + response +",have a good day");
+}
+function showTopic(){
+var x = document.getElementById('demo');
+x.style.fontSize = "25px";
+x.style.color = "red";
+}
+
+function add(a,b){
+return a + b;
+}
+console.log(add(20,30));
+console.log(add(18,17));
